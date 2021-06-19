73deaf20e5726bb32dfc83e27c7cebeba27c61fe
emilyalv <emilyaalv@gmail.com>
2021-06-19 10:49:02 -0500

    added the routes for the stats page and exercise page, added put request to update shell workout records, added get request for the stats page

--- routes/api.js
+++ routes/api.js
@@ -18,16 +18,29 @@ router.post('/api/workouts', ( { workoutData }, res) => {
     .catch(err => {
       res.status(400).json(err);
     });
-
-    // try {
-    //     const newWorkout = await Workout. {
-
-    //     };
-    //     res.status(200).json(newWorkout);
-    // } catch (error) {
-    //     res.status(400).json(error);
-    // }
-     
 });
+
+router.put('/api/workouts/:id', async (req, res) => {
+  try {
+    const exercise = await Workout.findByIdAndUpdate(req.params.id, {
+      $push: {
+        exercises: req.body
+      }
+    })
+    res.json(exercise)
+  }
+  catch (error) {
+    res.status(400).json(error);
+}
+});
+
+router.get('/api/workouts/range', async (req,res) => {
+  try {
+    const workouts = await Workout.find({}).sort({day:-1}).limit(7);
+    res.status(200).json(workouts);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+})
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path")
 
 const PORT = process.env.PORT || 3000;
 
@@ -16,9 +17,18 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useUnifiedTopology: true 
 });
 
+app.get('/stats', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/stats.html'));
+});
+
+app.get('/exercise', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/exercise.html'));
+});
+
 // routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
+

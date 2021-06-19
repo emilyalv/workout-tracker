b5695c5d40d1770a7e1012f083703fc805a5ff8c
emilyalv <emilyaalv@gmail.com>
2021-06-19 14:16:05 -0500

    deploying

--- server.js
+++ server.js
@@ -11,11 +11,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true 
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/myFirstDatabase',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 app.get('/stats', (req, res) => {
   res.sendFile(path.join(__dirname, './public/stats.html'));
